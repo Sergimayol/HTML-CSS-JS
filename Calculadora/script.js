2a50fed8ecc5d9395c40b9146252700a996e4eaa
Sergi Mayol <83218617+Sergimayol@users.noreply.github.com>
2022-03-14 15:43:25 +0100

    not finished

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,9 +1,1 @@
-function calculate() {
-    const element = document.getElementsByClassName('screen');
-    console.log(element); // 👉️ div#box
 
-    // ✅ Works
-    element.textContent = 'New content';
-}
-
-calculate();
